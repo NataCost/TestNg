f56abb1297052db587b05b0c5838f1ba9c676f4d
NataCost <nataliyacost@gmail.com>
2016-03-03 19:58:01 +0200

    Initial commit

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by Nata_Cost on 03.03.2016.
+ */
+public class TestRunner {
+    @Test
+    public void ThisIsTestMethod1(){
+        System.out.println("Method 1");
+
+    }
+    @Test
+    public void ThisIsTestMethod2(){
+        System.out.println("Method 2");
+    }
+    @Test
+    public void ThisIsTestMethod3(){
+        System.out.println("Method 3");
+
+    }
+
+}
